d70f951afce053aa56a1002dc25689ffa8d7bfc1
Matthew Williamson <chuwilliamson@gmail.com>
2016-11-06 14:27:47 -0600

    initial

---
+++ Practice/Abilities/Program.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="AIE">
+//   COPY
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Abilities
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        private static void Main()
+        {
+            var c = new Cactuar("cactus guy", 125, 25);
+            c.Cast("grenade", c); 
+            c.Cast("vanish"); 
+            c.Cast("heal"); 
+            Console.WriteLine("Program Complete..");
+            Console.ReadLine();
+        }
+    }
+}
