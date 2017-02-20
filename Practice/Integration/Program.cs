4c9efe18f373dc2722c36916951fe6760d3b2aca
Matthew Williamson <chuwilliamson@gmail.com>
2017-02-20 15:47:17 -0600

    Clean Program

--- Practice/Integration/Program.cs
+++ Practice/Integration/Program.cs
@@ -1,68 +1,9 @@
-﻿using System;
-using System.Diagnostics;
-using Utilities;
-using Utilities.Serialization;
-using System.Xml.Serialization;
-using System.Xml;
-using System.IO;
-
-namespace Integration
+﻿namespace Integration
 {
-    public enum States
-    {
-        init = 0,
-        idle = 1,
-        exit = 2,
-    }
-   
-    [Serializable]
-    public class Unit
-    {
-        public int Health { get; set; }
-        public string Name { get; set; }
-        public int Resource { get; set; }
-    }
     class Program
-    {
-        public static readonly string savepath = Environment.CurrentDirectory + "../Saves/Json/";
-
-        static void Test1()
-        {
-
-            Unit u = new Unit() { Health = 5, Name = "Max", Resource = 25, };
-            XML.Save<Unit>("unit", u);
-            Unit b = XML.Load<Unit>("unit");
-        }
-
-        static void Test2()
-        {
-            FiniteStateMachine<States> fsm = new FiniteStateMachine<States>();
-            Action a = doit;
-            fsm.AddTransition(States.init, States.idle, a);
-            fsm.AddTransition(States.idle, States.idle, a);
-            fsm.AddTransition(States.init, States.exit, a);
-            
-            
-            fsm.Start(States.init);
-            
-            for(int i = 0; i < 25; i++)
-                fsm.ChangeState(i % 3);
-            Json.Save("gamefsminit100", fsm);
-          
-        }
-
+    { 
         static void Main(string[] arguments)
-        {
- 
-
-
-
-        }
-        static void doit() { }
-
-
-       
-
-
+        { 
+        } 
     }
 }
